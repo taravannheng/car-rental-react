b63045c2d8829bf7b666b7441878aec8ce1387a8
taravannheng <heng.t.taravann@gmail.com>
2022-05-08 15:52:23 +0700

    Change primary and text buttons to button element

--- src/pages/login/LogInPage.jsx
+++ src/pages/login/LogInPage.jsx
@@ -1,8 +1,4 @@
-import PrimaryButton from '../../components/buttons/PrimaryButton'
-import TextButton from '../../components/buttons/TextButton'
-import DividerWithText from '../../components/divider/DividerWithText'
-import TextInput from '../../components/forms/TextInput'
-import PasswordInput from '../../components/forms/PasswordInput'
+import LogInForm from "../../patterns/forms/LogInForm"
 
 function LoginPage() {
   return (
@@ -11,13 +7,7 @@ function LoginPage() {
       <div className="login__container">
         <div className="login__logo">
         </div>
-        <form className="form login__form">
-          <TextInput label="Email" placeholder="Enter your email..." isRequired={true} />
-          <PasswordInput label="Password" isRequired={true} />
-          <PrimaryButton>Log In</PrimaryButton>
-          <DividerWithText text="OR" />
-          <TextButton>Sign Up</TextButton>
-        </form>
+        <LogInForm />
       </div>
     </div>
   )
