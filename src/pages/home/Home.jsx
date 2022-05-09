fa48f2a054105473e1b81c64deee1ed8bc4355f9
taravannheng <heng.t.taravann@gmail.com>
2022-05-10 00:11:10 +0700

    Get image urls from firestore

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react'
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
+
 function Home() {
+  const [imgUrls, setImgUrls] = useState(null)
+
+  const db = getFirestore()
+
+  const docRef = doc(db, 'images', 'cars')
+
+  useEffect(() => {
+    getDoc(docRef).then((doc) => {
+      const { imgUrls } = doc.data()
+
+      setImgUrls(imgUrls)
+    })
+
+    // eslint-disable-next-line
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>home</div>
   )
 }
 
