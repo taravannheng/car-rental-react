46a1fc7f915982daa05953ba88d0a45189588b61
taravannheng <heng.t.taravann@gmail.com>
2022-05-12 02:11:22 +0700

    Update cart counter to use cart context

--- src/patterns/Header/Header.jsx
+++ src/patterns/Header/Header.jsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import CartContext from '../../contexts/cartContext'
 
 function Header() {
+  const { cart } = useContext(CartContext)
+
   return (
     <header className="header">
         <Link to='/home'><div className="header__logo" style={{ backgroundImage: `url(https://firebasestorage.googleapis.com/v0/b/car-rental-8cd87.appspot.com/o/logo-dark.png?alt=media&token=2a679b3e-2b30-4c4d-b59d-ee95f3515459)` }}></div></Link>    
@@ -12,7 +16,7 @@ function Header() {
         <Link to='/home' className='header__cart'>
           <span>Cart</span>
           <div className="counter">
-            <span className="counter__text">0</span>
+            <span className="counter__text">{cart.length}</span>
           </div>
         </Link>
     </header>
