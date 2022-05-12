a395a170807d807823f25fc7eeea6085f7e6ad18
taravannheng <heng.t.taravann@gmail.com>
2022-05-12 20:41:35 +0700

    Add condition and style for disabled checkout button

--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -4,9 +4,11 @@ import Header from "../../patterns/Header/Header"
 import CartContext from '../../contexts/cartContext'
 import { useContext } from 'react'
 import ReservationItem from '../../components/ReservationItem/ReservationItem'
+import CheckoutContext from '../../contexts/checkoutContext'
 
 function CartPage() {
   const { cart } = useContext(CartContext)
+  const { isValidToCheckout } = useContext(CheckoutContext)
 
   if (cart.length === 0) {
     return <Navigate to={ROUTES.HOME} />
@@ -28,7 +30,7 @@ function CartPage() {
         { cart.map(car => <ReservationItem productDetails={car} key={car.id}/>) }
         </div>
         <div className="cart__checkout">
-          <div><Link to={ROUTES.CHECKOUT} className='cart__checkout-button'>Checkout</Link></div>
+          <div><Link to={ROUTES.CHECKOUT} className={`cart__checkout-button ${!isValidToCheckout && 'cart__checkout-button--disabled'}`} style={{ pointerEvents: isValidToCheckout ? '' : 'none' }}>Checkout</Link></div>
         </div>
       </main>
     </>
