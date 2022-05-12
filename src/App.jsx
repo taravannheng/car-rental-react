d8443aebdd0a46538cf4f2c3c7bc692e5f5f94d8
taravannheng <heng.t.taravann@gmail.com>
2022-05-12 23:00:16 +0700

    Re-add sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import SignInPage from "./pages/Signin/SignInPage";
-import SignUpPage from "./pages/Signup/SignUpPage";
+import SignUpPage from './pages/Signup/SignUpPage';
 import ForgotPasswordPage from "./pages/ForgotPassword/ForgotPasswordPage"
 import CartPage from './pages/Cart/CartPage';
 import Home from "./pages/Home/Home";
