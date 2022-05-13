bea5c765845ad09780bc3dbbad0470e133e0c6a1
taravannheng <heng.t.taravann@gmail.com>
2022-05-13 10:59:46 +0700

    Add confirmation page

--- src/pages/Confirmation/ConfirmationPage.jsx
+++ src/pages/Confirmation/ConfirmationPage.jsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom'
+import * as ROUTES from '../../constants/routes'
+
 function ConfirmationPage() {
   return (
-    <div>ConfirmationPage</div>
+    <main className="confirmation">
+      <div className="confirmation__overlay"></div>
+      <div className="confirmation__dialog">
+        <h1 className="confirmation__dialog-title">Thank You!</h1>
+        <p className="confirmation__dialog-description">We will contact you through email for further processes.</p>
+        <Link to={ROUTES.HOME} className="confirmation__dialog-back-button">Back to Home</Link>
+      </div>
+    </main>
   )
 }
 
