4a391eaf37d2795a48def9f9dad46a01ee922588
taravannheng <heng.t.taravann@gmail.com>
2022-05-13 11:07:25 +0700

    Add not found page

--- src/pages/NotFound/NotFoundPage.jsx
+++ src/pages/NotFound/NotFoundPage.jsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom'
+import * as ROUTES from '../../constants/routes'
+
 function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
+    <main className="notfound">
+      <div className="notfound__overlay"></div>
+      <div className="notfound__dialog">
+        <h1 className="notfound__dialog-title">404!</h1>
+        <p className="notfound__dialog-description">Sorry, we cannot find the page you are looking for!</p>
+        <Link to={ROUTES.HOME} className="notfound__dialog-back-button">Back to Home</Link>
+      </div>
+    </main>
   )
 }
 
