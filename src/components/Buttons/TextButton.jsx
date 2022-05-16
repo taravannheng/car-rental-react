6a7802e50703ecc07f97c030a8c5d0d49859d35a
taravannheng <heng.t.taravann@gmail.com>
2022-05-16 22:05:50 +0700

    refactor(button): Remove text button component as it is not required
    Different button types will be merged into one button component and its styles will be controlled by css using class names

--- src/components/Buttons/TextButton.jsx
+++
@@ -1,7 +0,0 @@
-function TextButton({ children, isDisabled, isRequired }) {
-  return (
-    <button type="button" className={`button text-button ${isDisabled && 'button--disabled'}`}  required={isRequired && 'required'}>{children}</button>
-  )
-}
-
-export default TextButton
