a02128aa4495aa074c521e23d1d469990cf0ac25
taravannheng <heng.t.taravann@gmail.com>
2022-05-16 22:09:47 +0700

    refactor(button): Change button component name as all button types will use the same button component

--- src/components/Buttons/PrimaryButton.jsx
+++
@@ -1,7 +0,0 @@
-function PrimaryButton({ children, isDisabled }) {
-  return (
-    <button className={`button primary-button ${isDisabled && 'button--disabled'}`} type="button">{children}</button>
-  )
-}
-
-export default PrimaryButton
